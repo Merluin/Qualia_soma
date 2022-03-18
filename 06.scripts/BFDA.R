027e8936aa8a2bb30fee39dd468c339756d2d261
thomas QUETTIER <thomasquettier@MBP-di-thomas.lan>
2022-03-18 13:31:43 +0100

    FF

--- 06.scripts/BFDA.R
+++ 06.scripts/BFDA.R
@@ -3,11 +3,10 @@
 # Experiment:     Qualiasoma_binocular_rivalry
 # Programmer:     Thomas Quettier
 # Date:           16/06/2021
-# Description:    BFDA
+# Description:    open-ended BFDA
 #
 #################################################
 
-############### Parameters ----
 rm(list=ls())
 ## library ----
 library(dplyr)
@@ -21,65 +20,69 @@ library(BFDA)
 
 ## loading data ----
 
-load("04.data_preprocessing/qualia_soma.RData")
+load("04.data_preprocessing/BFDA.RData")
 
+
+## parametres ----
+
+ESH1          <-    0.478 #we can also provide a vector; rnorm(100000, 0.5, 0.1)
+ESH0          <-    0
+
+Type          <-    "t.paired" #expected.ES has to be provided as Cohen’s d
+Alternative   <-    "greater"
+
+Prior         <-    list("Cauchy",list(prior.location = 0,prior.scale = sqrt(2)/2)) #prior distribution for t-tests
+Design        <-    "sequential" # otherwise fixed-N 
+
+nmax          <-    300 #maximum sample size
+nmin          <-    20 #initial sample size
+
+Boundary      <-    6 # 6 is required by cortex
+
+rep           <-    1000
+Verbose       <-    TRUE 
+Cores         <-    11
+Stepsize      <-    10 #The number of observations added to the sample in each step of a sequential process
+
+
+## Simulation, both under H1 and under H0  ----
 # SIMULATION https://rawgit.com/nicebread/BFDA/master/package/doc/BFDA_manual.html
 
+sim.H1 <- BFDA.sim(expected.ES = ESH1, type = Type, prior = Prior, design = Design,
+                   n.max = nmax, alternative = Alternative,boundary = Boundary, 
+                   B = rep,verbose = Verbose, cores = Cores,stepsize = Stepsize)
 
-nmin<-40 #initial sample size
-nmax<-200 #maximum sample size
-
-sim.H1 <- BFDA.sim(expected.ES = 0.478, 
-                   type = "t.paired",
-                   prior = list("Cauchy",
-                                list(prior.location = 0,
-                                     prior.scale = sqrt(2)/2)),
-                   design = "sequential", 
-                   n.max = nmax, 
-                   alternative = "greater",
-                   boundary = Inf, 
-                   B = 10000,
-                   verbose = TRUE, 
-                   cores = 11, 
-                   stepsize = 10)
-
-nlz.H1 <- BFDA.analyze(sim.H1,
-                       design = "sequential",
-                       n.min = nmin,
-                       n.max = nmax, 
-                       boundary = 6)
-
-sim.H0 <- BFDA.sim(expected.ES = 0,  
-                   type = "t.paired",
-                   prior = list("Cauchy",
-                                list(prior.location = 0,
-                                     prior.scale = sqrt(2)/2)),
-                   design = "sequential", 
-                   n.max = nmax, 
-                   alternative = "greater", 
-                   boundary = Inf, 
-                   B = 10000,
-                   verbose = TRUE, 
-                   cores = 11, 
-                   stepsize = 10)
+sim.H0 <- BFDA.sim(expected.ES = ESH0, type = Type, prior = Prior, design = Design,
+                   n.max = nmax, alternative = Alternative,boundary = Boundary, 
+                   B = rep,verbose = Verbose, cores = Cores,stepsize = Stepsize) 
 
 
-nlz.H0 <- BFDA.analyze(sim.H0, 
-                       design="sequential", 
-                       n.min = nmin, 
-                       n.max = nmax, 
-                       boundary=6)
+## Analyze the simulated data ----
+
+nlz.H1 <- BFDA.analyze(sim.H1, design = Design, n.min = nmin,
+                       n.max = nmax, boundary = Boundary) 
+
+nlz.H0 <- BFDA.analyze(sim.H0,design = Design, n.min = nmin,
+                       n.max = nmax, boundary = Boundary)
 
 
 
+## Plots ----
 
 plot(sim.H1, n.min=nmin, n.max=nmax, boundary=c(1/6, 6))
 plot(sim.H0, n.min=nmin, n.max=nmax, boundary=c(1/6, 6), forH1 = FALSE)
 
-SSD(sim.H1, power=.80, boundary=c(1/6, 6))
-SSD(sim.H0, alpha=.02, boundary=c(1/6, 6))
+SSD(sim.H1, power=.90, boundary=c(1/Boundary, Boundary))
+SSD(sim.H0, alpha=.02, boundary=c(1/Boundary, Boundary))
 
 
 
+##save data ---
 
+save(sim.H1,sim.H0,nlz.H1,nlz.H0,file="04.data_preprocessing/BFDA.RData")
 
+#################################################
+# 
+# END
+#
+#################################################
